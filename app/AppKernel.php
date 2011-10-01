7ee63c86ef5f52a7dabcae398f2cfa971dd6aa34
Dustin Dobervich <ddobervich@gmail.com>
2011-10-01 11:54:40 -0500

    removed Behat bundle instantiation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,11 +36,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new JMS\DebuggingBundle\JMSDebuggingBundle($this);    
-            
-            if ('test' === $this->getEnvironment()) {
-                $bundles[] = new Behat\BehatBundle\BehatBundle();
-            }
+            $bundles[] = new JMS\DebuggingBundle\JMSDebuggingBundle($this);
         }
 
         return $bundles;
