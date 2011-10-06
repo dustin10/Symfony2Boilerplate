d65d07c94f2c153949b92ac071807710a799bc11
Dustin Dobervich <ddobervich@gmail.com>
2011-10-05 23:13:37 -0500

    added extension classes for bundles

---
+++ src/Acme/AdminBundle/DependencyInjection/AcmeAdminExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Acme\AdminBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class AcmeAdminExtension extends Extension
+{
+    /**
+     * Loads the extension.
+     * 
+     * @param array $configs The configuration
+     * @param ContainerBuilder $container The container builder
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        
+        $toLoad = array('admin.xml');
+        foreach ($toLoad as $file) {
+            $loader->load($file);
+        }
+    }
+}
+
